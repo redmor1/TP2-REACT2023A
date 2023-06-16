7b04637fe14c5f8bd66fabbb0746dc85338cdc6c
redmor1 <lightz17@hotmail.com>
2023-06-16 11:31:21 -0300

    Agregar peticion a API y mapeo de la informacion recibida

--- src/components/Personaje.jsx
+++ src/components/Personaje.jsx
@@ -1,17 +1,10 @@
-function Personaje() {
+function Personaje(props) {
   return (
     <div className="card col-3" style={{ style: "18rem" }}>
-      <img
-        src="https://placehold.co/600x400"
-        className="card-img-top"
-        alt="..."
-      />
+      <img src={props.image} className="card-img-top" alt="..." />
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card&apos;s content.
-        </p>
+        <h3 className="card-title">{props.name}</h3>
+        <h5 className="card-title">{props.species}</h5>
         <a href="#" className="btn btn-primary">
           Go somewhere
         </a>
--- src/screens/Personajes.jsx
+++ src/screens/Personajes.jsx
@@ -1,15 +1,41 @@
 import Navbar from "../components/Navbar";
 import Personaje from "../components/Personaje";
+import { useState } from "react";
 
 function Personajes() {
+  const [characters, setCharacters] = useState();
+
+  async function getCharacters() {
+    try {
+      const res = await fetch("https://rickandmortyapi.com/api/character");
+      const json = await res.json();
+      setCharacters(json.results);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  getCharacters();
+
   return (
     <>
       <Navbar />
       <div className="container row">
-        <Personaje />
-        <Personaje />
-        <Personaje />
-        <Personaje />
+        {" "}
+        {characters ? (
+          characters.map((character) => {
+            return (
+              <Personaje
+                key={character.id}
+                image={character.image}
+                name={character.name}
+                species={character.species}
+              />
+            );
+          })
+        ) : (
+          <h1>Cargando...</h1>
+        )}
       </div>
     </>
   );
