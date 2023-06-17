29d1bdd545fbd37e08ebe44d63cd7cd55b21fbca
redmor1 <lightz17@hotmail.com>
2023-06-17 11:50:21 -0300

    Agregar Link a componente Ubicacion

--- src/components/Ubicacion.jsx
+++ src/components/Ubicacion.jsx
@@ -1,13 +1,22 @@
+import { Link } from "react-router-dom";
+
 function Ubicacion(props) {
   return (
     <div className="card col-3">
-      <div className="card-body">
-        <h5 className="card-title">{props.name}</h5>
-        <h6 className="card-subtitle mb-2 text-body-secondary">{props.type}</h6>
-        <h6 className="card-subtitle mb-2 text-body-secondary">
-          {props.dimension}
-        </h6>
-      </div>
+      <Link
+        to={`/ubicaciones/${props.id}`}
+        className="text-decoration-none text-reset"
+      >
+        <div className="card-body">
+          <h5 className="card-title">{props.name}</h5>
+          <h6 className="card-subtitle mb-2 text-body-secondary">
+            {props.type}
+          </h6>
+          <h6 className="card-subtitle mb-2 text-body-secondary">
+            {props.dimension}
+          </h6>
+        </div>
+      </Link>
     </div>
   );
 }
