cb85d9fc34df21def436622104db6feecdb5f069
redmor1 <lightz17@hotmail.com>
2023-06-17 11:52:16 -0300

    Agregar screen UbicacionDetallesScreen.jsx

---
+++ src/screens/UbicacionDetallesScreen.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+import UbicacionDetalles from "../components/UbicacionDetalles";
+
+function UbicacionDetallesScreen() {
+  return (
+    <>
+      <Navbar />
+      <UbicacionDetalles />
+    </>
+  );
+}
+
+export default UbicacionDetallesScreen;
