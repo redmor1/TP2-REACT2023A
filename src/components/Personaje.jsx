b44dd1176ca71cc428005b83072d24740d82a4dc
redmor1 <lightz17@hotmail.com>
2023-06-17 11:55:48 -0300

    Eliminar boton y agregar Link en componente Personaje

--- src/components/Personaje.jsx
+++ src/components/Personaje.jsx
@@ -4,14 +4,16 @@ function Personaje(props) {
   console.log(props);
   return (
     <div className="card col-3">
-      <img src={props.image} className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h3 className="card-title">{props.name}</h3>
-        <h5 className="card-title">{props.species}</h5>
-        <Link to={`/personajes/${props.id}`} className="btn btn-primary">
-          Ver detalles
-        </Link>
-      </div>
+      <Link
+        to={`/personajes/${props.id}`}
+        className="text-decoration-none text-reset"
+      >
+        <img src={props.image} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h3 className="card-title">{props.name}</h3>
+          <h5 className="card-title">{props.species}</h5>
+        </div>
+      </Link>
     </div>
   );
 }
