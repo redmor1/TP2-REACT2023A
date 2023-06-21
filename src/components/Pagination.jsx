1e4afd2a6340976178adcd82eb865de203af415c
redmor1 <lightz17@hotmail.com>
2023-06-20 21:14:34 -0300

    Modificar apariencia de componente Pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,18 +1,28 @@
 function Pagination(props) {
   return (
-    <nav aria-label="Page navigation example">
-      <ul className="pagination">
+    <nav aria-label="Page navigation">
+      <ul className="pagination pagination-lg justify-content-center my-5">
         <li className="page-item">
-          <button className="page-link" onClick={props.prevPage} href="#">
-            Previous
+          <button
+            className="page-link"
+            onClick={props.prevPage}
+            aria-label="Previous"
+          >
+            <span aria-hidden="true">&lt;</span>
           </button>
         </li>
         <li className="page-item">
-          <button className="page-link">{props.currentPage}</button>
+          <span className="page-link text-reset text-decoration-none">
+            Pagina {props.currentPage}
+          </span>
         </li>
         <li className="page-item">
-          <button className="page-link" onClick={props.nextPage}>
-            Next
+          <button
+            className="page-link"
+            onClick={props.nextPage}
+            aria-label="Previous"
+          >
+            <span aria-hidden="true">&gt;</span>
           </button>
         </li>
       </ul>
