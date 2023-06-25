afc85607f4ac7328578df2a388fa65887313bbef
redmor1 <lightz17@hotmail.com>
2023-06-24 22:01:52 -0300

    Modificar PersonajesScreen para utilizar el hook ...DataFetcher

--- src/screens/PersonajesScreen.jsx
+++ src/screens/PersonajesScreen.jsx
@@ -2,47 +2,18 @@ import Navbar from "../components/Navbar";
 import Personaje from "../components/Personaje";
 import Error from "../components/Error";
 import Loader from "../components/Loader";
-import { useState, useEffect } from "react";
+import { useRickAndMortyDataFetcher } from "../hooks/useRickAndMortyDataFetcher";
 import Pagination from "../components/Pagination";
 
 function Personajes() {
-  const [characters, setCharacters] = useState();
-  const [error, setError] = useState();
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState();
-
-  async function getCharacters(page) {
-    try {
-      const res = await fetch(
-        `https://rickandmortyapi.com/api/character?page=${page}`
-      );
-      const json = await res.json();
-      if (json.error) {
-        setError(json.error);
-      }
-      setCharacters(json.results);
-      setTotalPages(json.info.pages);
-    } catch (e) {
-      setError(e.message);
-      console.log(e);
-    }
-  }
-
-  function nextPage() {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-    }
-  }
-
-  function prevPage() {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  }
-
-  useEffect(() => {
-    getCharacters(currentPage);
-  }, [currentPage]);
+  const {
+    data: characters,
+    error,
+    currentPage,
+    totalPages,
+    nextPage,
+    prevPage,
+  } = useRickAndMortyDataFetcher("https://rickandmortyapi.com/api/character");
 
   if (error) {
     return (
