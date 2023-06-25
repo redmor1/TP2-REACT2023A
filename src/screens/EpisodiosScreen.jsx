7eecd9799981bcde450930282e8c7e45e8ba6c18
redmor1 <lightz17@hotmail.com>
2023-06-24 21:15:43 -0300

    Modificar EpisodiosScreen para utilizar el custom hook UseRickAndMortyDataFetcher

--- src/screens/EpisodiosScreen.jsx
+++ src/screens/EpisodiosScreen.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useRickAndMortyDataFetcher } from "../hooks/useRickAndMortyDataFetcher";
 import Episodio from "../components/Episodio";
 import Navbar from "../components/Navbar";
 import Error from "../components/Error";
@@ -6,43 +6,14 @@ import Loader from "../components/Loader";
 import Pagination from "../components/Pagination";
 
 function EpisodiosScreen() {
-  const [episodios, setEpisodios] = useState();
-  const [error, setError] = useState();
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState();
-
-  async function getEpisodes(page) {
-    try {
-      const res = await fetch(
-        `https://rickandmortyapi.com/api/episode?page=${page}`
-      );
-      const json = await res.json();
-      if (json.error) {
-        setError(json.error);
-      }
-      setEpisodios(json.results);
-      setTotalPages(json.info.pages);
-    } catch (e) {
-      setError(e.message);
-      console.log(e);
-    }
-  }
-
-  function nextPage() {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-    }
-  }
-
-  function prevPage() {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  }
-
-  useEffect(() => {
-    getEpisodes(currentPage);
-  }, [currentPage]);
+  const {
+    data: episodios,
+    error,
+    currentPage,
+    totalPages,
+    nextPage,
+    prevPage,
+  } = useRickAndMortyDataFetcher();
 
   if (error) {
     return (
